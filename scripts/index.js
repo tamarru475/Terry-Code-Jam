b2cb5a106653ee9c6a5a6d5e93c78f725d9ae610
Practicum AmitBG217 <amit217@yandex.com>
2022-05-24 12:09:23 +0300

    style: cards logo

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,21 @@ import { Card, confCard } from "./Card.js";
 const cardData = [
   {
     title: "Hello World !",
-    image: "Link",
+    image: "../images/Terry20items.png",
     description: "Description",
   },
   {
     title: "Hello World !",
-    image: "Link",
+    image: "../images/Terry20items.png",
+    description: "Description",
+  },
+  {
+    title: "Hello World !",
+    image: "../images/Terry20items.png",
     description: "Description",
   },
 ];
-const cardSection = document.querySelector(".cards");
+const cardSection = document.querySelector(".cards__container");
 cardData.forEach((card) => {
   const newCard = new Card(card, confCard.cardTemplate);
   const cardElement = newCard.generateCard();
